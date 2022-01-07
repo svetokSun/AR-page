d451298e486c7a27995b623c50b3a16ae8c785bb
svetokSun <79097116+svetokSun@users.noreply.github.com>
2022-01-08 02:46:32 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# AR-gift
+
+
+
+## for what
+
+To present  a gift with AR-technology. Project use simple tools to create path to gift.
+
+## Resources
+1. Gitlab Pages
+2. Java Script, react
+3. AR.js Marker Training
+
+## how it works
+To use it, open [this page]()
---
+++ ar-navigate.js
@@ -0,0 +1,10 @@
+<a-scene embedded arjs>
+  <a-marker preset="hiro">
+    <a-entity
+      rotation="-90 0 0"
+      scale="0.1 0.1 0.1"
+      gltf-model="https://cdn.aframe.io/test-models/models/glTF-2.0/virtualcity/VC.gltf">
+    </a-entity>
+  </a-marker>
+  <a-entity camera></a-entity>
+</a-scene>
